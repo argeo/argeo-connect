c8b1435e4c9e79ca4506cad08f7ce17ad09885bc
Mathieu Baudier <mbaudier@argeo.org>
2008-02-29 08:49:42 +0000

    Add GPL license
    git-svn-id: https://svn.argeo.org/connect/trunk@974 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.connect.research.stats/src/main/R/class/all.R
+++ org.argeo.connect.research.stats/src/main/R/class/all.R
@@ -1,2 +1,17 @@
+# Copyright 2008 Mathieu Baudier
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 source("src/main/R/class/bestPca.R")
 source("src/main/R/class/eGvt2008.R")
