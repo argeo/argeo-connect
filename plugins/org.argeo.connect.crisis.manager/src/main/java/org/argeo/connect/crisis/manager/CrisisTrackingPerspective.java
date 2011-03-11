8ea3a19074200980c43bb8145e176ba0ba3d06f0
Mathieu Baudier <mbaudier@argeo.org>
2011-03-11 09:59:50 +0000

    Improve JCR and GIS
    git-svn-id: https://svn.argeo.org/connect/trunk@4283 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- plugins/org.argeo.connect.crisis.manager/src/main/java/org/argeo/connect/crisis/manager/CrisisTrackingPerspective.java
+++ plugins/org.argeo.connect.crisis.manager/src/main/java/org/argeo/connect/crisis/manager/CrisisTrackingPerspective.java
@@ -1,6 +1,6 @@
 package org.argeo.connect.crisis.manager;
 
-import org.argeo.gis.ui.views.DataStoresView;
+import org.argeo.gis.ui.views.FeatureSourcesView;
 import org.argeo.gis.ui.views.LayersView;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
@@ -8,7 +8,6 @@ import org.eclipse.ui.IPerspectiveFactory;
 
 public class CrisisTrackingPerspective implements IPerspectiveFactory {
 
-	@Override
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(true);
@@ -17,8 +16,8 @@ public class CrisisTrackingPerspective implements IPerspectiveFactory {
 		IFolderLayout topLeft = layout.createFolder("topLeft",
 				IPageLayout.LEFT, 0.3f, editorArea);
 		topLeft.addView(LayersView.ID);
-		topLeft.addView(DataStoresView.ID);
-		topLeft.addView("org.argeo.jcr.ui.explorer.browserView");
+		topLeft.addView(FeatureSourcesView.ID);
+		//topLeft.addView("org.argeo.jcr.ui.explorer.browserView");
 	}
 
 }
---
+++ runtime/org.argeo.connect.core/src/main/java/org/argeo/connect/ConnectNames.java
@@ -0,0 +1,11 @@
+package org.argeo.connect;
+
+public interface ConnectNames {
+	public final static String CONNECT_ = "connect:";
+
+	// situation
+	public final static String CONNECT_PRIORITY = "connect:priority";
+	public final static String CONNECT_STATUS = "connect:status";
+	public final static String CONNECT_UPDATE = "connect:update";
+	public final static String CONNECT_TEXT = "connect:text";
+}
