6b6d7a7a8d4f3f224fe86693f5054d6bbbd606d3
Bruno Sinou <bsinou@argeo.org>
2011-04-01 10:17:47 +0000

    Inroduce UI GPS
    git-svn-id: https://svn.argeo.org/connect/trunk@4414 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ plugins/org.argeo.connect.ui.gps/src/main/java/org/argeo/connect/ui/gps/GpsPerspective.java
@@ -0,0 +1,21 @@
+package org.argeo.connect.ui.gps;
+
+import org.argeo.connect.ui.gps.views.GpsView;
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class GpsPerspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
+		layout.setEditorAreaVisible(true);
+		layout.setFixed(false);
+
+		IFolderLayout topLeft = layout.createFolder("topLeft",
+				IPageLayout.LEFT, 0.3f, editorArea);
+		topLeft.addView(GpsView.ID);
+
+	}
+
+}
---
+++ plugins/org.argeo.connect.ui.gps/src/main/java/org/argeo/connect/ui/gps/views/GpsView.java
@@ -0,0 +1,7 @@
+package org.argeo.connect.ui.gps.views;
+
+import org.argeo.eclipse.ui.jcr.explorer.JcrExplorerView;
+
+public class GpsView extends JcrExplorerView {
+public final static String ID = "org.argeo.connect.ui.gps.gpsBrowserView";
+}
