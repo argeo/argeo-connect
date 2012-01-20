ffd157847938696e2209a83b15a2d0db558b4f3b
Bruno Sinou <bsinou@argeo.org>
2012-01-20 11:08:24 +0000

    Slightly modify boundary values for clean parameters
    git-svn-id: https://svn.argeo.org/connect/trunk@4998 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- runtime/org.argeo.connect.core/src/main/java/org/argeo/connect/gpx/utils/JcrSessionUtils.java
+++ runtime/org.argeo.connect.core/src/main/java/org/argeo/connect/gpx/utils/JcrSessionUtils.java
@@ -1,6 +1,7 @@
 package org.argeo.connect.gpx.utils;
 
 import javax.jcr.Node;
+import javax.jcr.NodeIterator;
 import javax.jcr.Property;
 
 import org.argeo.ArgeoException;
@@ -21,7 +22,6 @@ public class JcrSessionUtils implements ConnectTypes, ConnectNames {
 
 			// TODO : remove hard coding from instantiation of default model.
 			Node tmpNode;
-
 			// maximal speed
 			tmpNode = newSession.addNode(
 					ConnectConstants.CONNECT_PARAM_SPEED_MAX,
@@ -29,9 +29,9 @@ public class JcrSessionUtils implements ConnectTypes, ConnectNames {
 			tmpNode.setProperty(Property.JCR_DESCRIPTION,
 					"Maximal acceptable speed value ");
 			tmpNode.setProperty(CONNECT_PARAM_MIN_VALUE, 0d);
-			tmpNode.setProperty(CONNECT_PARAM_MAX_VALUE, 250d);
+			tmpNode.setProperty(CONNECT_PARAM_MAX_VALUE, 500d);
 			// default value
-			tmpNode.setProperty(CONNECT_PARAM_VALUE, 100d);
+			tmpNode.setProperty(CONNECT_PARAM_VALUE, 200d);
 
 			// Maximal acceleration
 			tmpNode = newSession.addNode(
@@ -40,10 +40,10 @@ public class JcrSessionUtils implements ConnectTypes, ConnectNames {
 			tmpNode.setProperty(Property.JCR_DESCRIPTION,
 					"Maximal acceptable acceleration value ");
 			tmpNode.setProperty(CONNECT_PARAM_MIN_VALUE, 0d);
-			tmpNode.setProperty(CONNECT_PARAM_MAX_VALUE, 5d);
+			tmpNode.setProperty(CONNECT_PARAM_MAX_VALUE, 10d);
 			tmpNode.setProperty(CONNECT_PARAM_VALUE, 2d);
 
-			// Maximal radial speed
+			// Maximal rotation speed
 			tmpNode = newSession.addNode(
 					ConnectConstants.CONNECT_PARAM_ROTATION_MAX,
 					CONNECT_CLEAN_PARAMETER);
@@ -53,13 +53,54 @@ public class JcrSessionUtils implements ConnectTypes, ConnectNames {
 			tmpNode.setProperty(CONNECT_PARAM_MAX_VALUE, 360d);
 			tmpNode.setProperty(CONNECT_PARAM_VALUE, 90d);
 
+			tmpNode.getSession().save();
+			
 		} catch (Exception e) {
 			throw new ArgeoException(
 					"Cannot create new session for node named [" + name + "]",
 					e);
 		}
-
 		return newSession;
+	}
+
+	public static void copyOneParameterNode(Node paramModelNode,
+			Node paramTargetNode) throws Exception {
+		paramTargetNode.setProperty(Property.JCR_DESCRIPTION, paramModelNode
+				.getProperty(Property.JCR_DESCRIPTION).getString());
+		paramTargetNode.setProperty(CONNECT_PARAM_MIN_VALUE, paramModelNode
+				.getProperty(CONNECT_PARAM_MIN_VALUE).getDouble());
+		paramTargetNode.setProperty(CONNECT_PARAM_MAX_VALUE, paramModelNode
+				.getProperty(CONNECT_PARAM_MAX_VALUE).getDouble());
+		paramTargetNode.setProperty(CONNECT_PARAM_VALUE, paramModelNode
+				.getProperty(CONNECT_PARAM_VALUE).getDouble());
+	}
+
+	public static void copyDataFromModel(Node modelNode, Node newNode) {
+		try {
+
+			// Copy parameter nodes
+			NodeIterator ni = modelNode.getNodes();
+
+			while (ni.hasNext()) {
+				Node modParamNode = ni.nextNode();
+				if (modParamNode
+						.isNodeType(ConnectTypes.CONNECT_CLEAN_PARAMETER)) {
+					Node newParamNode;
+					if (newNode.hasNode(modParamNode.getName())) {
+						newParamNode = newNode.getNode(modParamNode.getName());
+					} else {
+						newParamNode = newNode.addNode(modParamNode.getName(),
+								modParamNode.getPrimaryNodeType().toString());
+					}
+					copyOneParameterNode(modParamNode, newParamNode);
+				}
+			}
+			newNode.getSession().save();
+		} catch (Exception e) {
+			throw new ArgeoException(
+					"Unexpected error while dupplicating infos from model to newly created node.",
+					e);
+		}
 	}
 
 	public static Node createLocalRepository(Node parent, String techName,
