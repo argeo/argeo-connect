15e840102eec4e0b4856c8d03fb5f8db928b6abf
Mathieu Baudier <mbaudier@argeo.org>
2012-02-16 11:25:14 +0000

    Adapt Connect GPS to RAP
    git-svn-id: https://svn.argeo.org/connect/trunk@5080 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- plugins/org.argeo.gis.ui.rap.openlayers/src/main/java/org/argeo/gis/ui/rap/openlayers/OpenLayersMapViewer.java
+++ plugins/org.argeo.gis.ui.rap.openlayers/src/main/java/org/argeo/gis/ui/rap/openlayers/OpenLayersMapViewer.java
@@ -55,6 +55,7 @@ import org.polymap.openlayers.rap.widget.layers.WMSLayer;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
 
@@ -301,8 +302,14 @@ public class OpenLayersMapViewer extends AbstractMapViewer implements
 					MultiPolygon mp = (MultiPolygon) geom;
 					List<PointGeometry> points = new ArrayList<PointGeometry>();
 					for (Coordinate coo : mp.getCoordinates()) {
-						// if (log.isDebugEnabled())
-						// log.debug(" " + coo.x + "," + coo.y);
+						points.add(new PointGeometry(coo.x, coo.y));
+					}
+					vf = new VectorFeature(new LineStringGeometry(
+							points.toArray(new PointGeometry[points.size()])));
+				} else if (geom instanceof LineString) {
+					LineString lineString = (LineString) geom;
+					List<PointGeometry> points = new ArrayList<PointGeometry>();
+					for (Coordinate coo : lineString.getCoordinates()) {
 						points.add(new PointGeometry(coo.x, coo.y));
 					}
 					vf = new VectorFeature(new LineStringGeometry(
