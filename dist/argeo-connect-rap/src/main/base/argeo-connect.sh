aeb351aa6a78dd487e93cd848871880f7d6eaf6c
Mathieu Baudier <mbaudier@argeo.org>
2012-02-17 11:57:58 +0000

    Refine deployment
    git-svn-id: https://svn.argeo.org/connect/trunk@5089 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- dist/argeo-connect-rap/src/main/base/argeo-connect.sh
+++ dist/argeo-connect-rap/src/main/base/argeo-connect.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 java -Dosgi.framework=lib/org.eclipse.osgi-3.6.2.jar \
  -jar lib/org.eclipse.equinox.launcher-1.1.1.jar \
- -console -configuration conf -data data -clean
+ -configuration conf -data data -clean &
