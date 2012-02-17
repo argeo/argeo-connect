892bba70d062d87ec0e6ad557d9fd4ae4b6832d1
Mathieu Baudier <mbaudier@argeo.org>
2012-02-17 14:51:39 +0000

    Increase memory
    git-svn-id: https://svn.argeo.org/connect/trunk@5093 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- dist/argeo-connect-rap/src/main/base/argeo-connect.sh
+++ dist/argeo-connect-rap/src/main/base/argeo-connect.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-java -Dosgi.framework=lib/org.eclipse.osgi-3.6.2.jar \
+java -Xmx1024m -Dosgi.framework=lib/org.eclipse.osgi-3.6.2.jar \
  -jar lib/org.eclipse.equinox.launcher-1.1.1.jar \
  -configuration conf -data data -clean &
