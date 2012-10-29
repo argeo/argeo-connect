6e6351628f5fc60ad27e7186bca7b3e628a5c2e2
Mathieu Baudier <mbaudier@argeo.org>
2012-10-29 17:41:53 +0000

    Introduce Connect Demo
    git-svn-id: https://svn.argeo.org/connect/trunk@5676 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ demo/plugins/org.argeo.connect.demo.rap/src/main/java/org/argeo/connect/demo/rap/GrEntryPoint.java
@@ -0,0 +1,11 @@
+package org.argeo.connect.demo.rap;
+
+import org.argeo.security.ui.rap.SecureEntryPoint;
+
+/**
+ * This class controls all aspects of the application's execution and is
+ * contributed through the plugin.xml.
+ */
+public class GrEntryPoint extends SecureEntryPoint {
+
+}
