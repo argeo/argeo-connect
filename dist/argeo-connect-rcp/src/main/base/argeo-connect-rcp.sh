d5929922730af6ee81f04691d6455db069590754
Mathieu Baudier <mbaudier@argeo.org>
2012-12-24 00:03:58 +0000

    Packaging
    git-svn-id: https://svn.argeo.org/connect/trunk@6006 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- dist/argeo-connect-rcp/src/main/base/argeo-connect-rcp.sh
+++ dist/argeo-connect-rcp/src/main/base/argeo-connect-rcp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 java -Dosgi.framework=lib/org.eclipse.osgi-3.6.2.jar \
- -jar lib/org.eclipse.equinox.launcher-1.1.1.jar \
+ -jar lib/org.eclipse.equinox.launcher-1.1.1.R36x_v20101122_1400.jar \
  -console -configuration conf -data data -clean
