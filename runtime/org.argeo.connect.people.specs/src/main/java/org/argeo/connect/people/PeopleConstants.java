4ff185c05783db051e891fb9c2f47603f93225d1
Bruno Sinou <bsinou@argeo.org>
2013-06-29 14:15:35 +0000

    first draft of a model for persons, organisations and groups.
    git-svn-id: https://svn.argeo.org/connect/trunk@6370 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- runtime/org.argeo.connect.people.specs/src/main/java/org/argeo/connect/people/PeopleConstants.java
+++ runtime/org.argeo.connect.people.specs/src/main/java/org/argeo/connect/people/PeopleConstants.java
@@ -2,4 +2,11 @@ package org.argeo.connect.people;
 
 public interface PeopleConstants {
 	public final static String PEOPLE_BASE_PATH = "/people:system";
+	
+	/*
+	 * USER ROLES
+	 */
+	public final static Integer ROLE_CONSULTANT = 0;
+	public final static Integer ROLE_MANAGER = 1;
+	public final static Integer ROLE_ADMIN = 2;
 }
