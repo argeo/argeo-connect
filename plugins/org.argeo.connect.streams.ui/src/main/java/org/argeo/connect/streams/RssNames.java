31cafe662a78847552bd8e974eda0df1b7457434
Bruno Sinou <bsinou@argeo.org>
2013-09-06 15:15:06 +0000

    first draft for model.
    git-svn-id: https://svn.argeo.org/connect/trunk@6444 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- plugins/org.argeo.connect.streams.ui/src/main/java/org/argeo/connect/streams/RssNames.java
+++ plugins/org.argeo.connect.streams.ui/src/main/java/org/argeo/connect/streams/RssNames.java
@@ -1,5 +1,19 @@
 package org.argeo.connect.streams;
 
+import javax.jcr.Property;
+
+/** Property Names for streams concepts.*/
 public interface RssNames {
 
+	//
+	public final static String RSS_TITLE = Property.JCR_TITLE;
+	public final static String RSS_DESCRIPTION = Property.JCR_DESCRIPTION;
+	public final static String RSS_LINK = "rss:link";
+
+	// ITEM
+	public final static String RSS_CATEGORY = "rss:category";
+	// Typically an image
+	public final static String RSS_ENCLOSURE = "rss:enclosure";
+	public final static String RSS_PUB_DATE = "rss:pubDate";
+	public final static String RSS_UPDATE_DATE = " rss:updatedate";
 }
