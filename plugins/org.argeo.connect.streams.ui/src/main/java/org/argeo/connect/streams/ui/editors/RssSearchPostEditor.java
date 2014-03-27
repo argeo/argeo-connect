8a496ebc01ce013f7731e986f281b2fa1ef2ea24
Bruno Sinou <bsinou@argeo.org>
2014-03-27 19:37:26 +0000

    Fix maven build pb.
    git-svn-id: https://svn.argeo.org/connect/trunk@6914 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- plugins/org.argeo.connect.streams.ui/src/main/java/org/argeo/connect/streams/ui/editors/RssSearchPostEditor.java
+++ plugins/org.argeo.connect.streams.ui/src/main/java/org/argeo/connect/streams/ui/editors/RssSearchPostEditor.java
@@ -8,14 +8,16 @@ import org.argeo.connect.streams.ui.providers.RssListLabelProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
 
 /**
- * A simple list of all posts with a search
+ * A simple list of all posts with a search. 
+ * 
+ * This must be fixed and the dependency to people should be removed.
  */
 public class RssSearchPostEditor extends SearchEntityEditor {
 	final static Log log = LogFactory.getLog(RssSearchPostEditor.class);
 	public final static String ID = RssUiPlugin.PLUGIN_ID
 			+ ".rssSearchPostEditor";
 
-	@Override
+	// @Override
 	protected ILabelProvider getCurrentLabelProvider() {
 		return new RssListLabelProvider(false);
 	}
