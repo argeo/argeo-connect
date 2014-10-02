f164a3487899faf42999b45822c6b9d287449072
Mathieu Baudier <mbaudier@argeo.org>
2014-10-02 19:49:10 +0000

    16px row data
    git-svn-id: https://svn.argeo.org/connect/trunk@7279 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.cms/src/org/argeo/cms/CmsUtils.java
+++ org.argeo.cms/src/org/argeo/cms/CmsUtils.java
@@ -1,9 +1,12 @@
 package org.argeo.cms;
 
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowData;
 
 /** Static utilities for the CMS framework. */
 public class CmsUtils {
+	public static RowData ROW_DATA_16px = new RowData(16, 16);
+
 	public static GridLayout noSpaceGridLayout() {
 		return noSpaceGridLayout(new GridLayout());
 	}
