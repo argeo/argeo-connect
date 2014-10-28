0c35787fd4ac4081f0bceaaa38c4135d4b6fcdda
Bruno Sinou <bsinou@argeo.org>
2014-10-28 10:15:24 +0000

    Remove unused and already partially deleted command.
    git-svn-id: https://svn.argeo.org/connect/trunk@7377 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.connect.people.rap/src/org/argeo/connect/people/rap/commands/AddEntityReferences.java
+++ org.argeo.connect.people.rap/src/org/argeo/connect/people/rap/commands/AddEntityReferences.java
@@ -1,26 +1,5 @@
 package org.argeo.connect.people.rap.commands;
 
-import javax.jcr.Node;
-import javax.jcr.Repository;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
-
-import org.argeo.connect.people.PeopleException;
-import org.argeo.connect.people.PeopleService;
-import org.argeo.connect.people.PeopleTypes;
-import org.argeo.connect.people.rap.PeopleRapPlugin;
-import org.argeo.connect.people.rap.dialogs.CreateEntityRefWithPositionDialog;
-import org.argeo.connect.people.rap.editors.utils.AbstractEntityEditor;
-import org.argeo.connect.people.rap.wizards.AddEntityReferenceWizard;
-import org.argeo.jcr.JcrUtils;
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Opens a wizard to add references to one or more entities to a given Parent
@@ -29,85 +8,89 @@ import org.eclipse.ui.handlers.HandlerUtil;
  * of the name of its parent.
  */
 @Deprecated
-public class AddEntityReferences extends AbstractHandler {
-	// private final static Log log =
-	// LogFactory.getLog(AddEntityReferences.class);
+public class AddEntityReferences {}
 
-	public final static String ID = PeopleRapPlugin.PLUGIN_ID
-			+ ".addEntityReferences";
-	public final static ImageDescriptor DEFAULT_IMG_DESCRIPTOR = PeopleRapPlugin
-			.getImageDescriptor("icons/add.png");
-	public final static String DEFAULT_LABEL = "Add...";
-	public final static String PARAM_REFERENCING_JCR_ID = "param.referencingJcrId";
-	public final static String PARAM_REFERENCED_JCR_ID = "param.referencedJcrId";
 
-	/* DEPENDENCY INJECTION */
-	private PeopleService peopleService;
-	private Repository repository;
-
-	public Object execute(final ExecutionEvent event) throws ExecutionException {
-
-		String referencingJcrId = event.getParameter(PARAM_REFERENCING_JCR_ID);
-		String referencedJcrId = event.getParameter(PARAM_REFERENCED_JCR_ID);
-
-		Session session = null;
-		try {
-			session = repository.login();
-			Node referencing = null;
-			if (referencingJcrId != null)
-				referencing = session.getNodeByIdentifier(referencingJcrId);
-
-			Node referenced = null;
-			if (referencedJcrId != null)
-				referenced = session.getNodeByIdentifier(referencedJcrId);
-
-			AddEntityReferenceWizard wizard = getCorrespondingWizard(event,
-					referencing, referenced);
-
-			if (wizard == null) {
-				Dialog diag = new CreateEntityRefWithPositionDialog(
-						HandlerUtil.getActiveShell(event), "Create position",
-						repository, peopleService, referencing, referenced,
-						PeopleTypes.PEOPLE_ORG);
-				diag.open();
-				return null;
-			}
-
-			WizardDialog dialog = new WizardDialog(
-					HandlerUtil.getActiveShell(event), wizard);
-			int result = dialog.open();
-
-			if (result == WizardDialog.OK) {
-				IEditorPart iep = HandlerUtil.getActiveWorkbenchWindow(event)
-						.getActivePage().getActiveEditor();
-				if (iep != null && iep instanceof AbstractEntityEditor)
-					((AbstractEntityEditor) iep).forceRefresh();
-			}
-		} catch (RepositoryException e) {
-			throw new PeopleException("unexpected JCR error while opening "
-					+ "editor for newly created programm", e);
-		} finally {
-			JcrUtils.logoutQuietly(session);
-		}
-		return null;
-	}
-
-	protected AddEntityReferenceWizard getCorrespondingWizard(
-			ExecutionEvent event, Node referencing, Node referenced)
-			throws RepositoryException {
-		if (referencing != null) {
-			if (referencing.isNodeType(PeopleTypes.PEOPLE_PERSON))
-				return null;
-		}
-		return null;
-	}
-
-	/* DEPENDENCY INJECTION */
-	public void setPeopleService(PeopleService peopleService) {
-		this.peopleService = peopleService;
-	}
-
-	public void setRepository(Repository repository) {
-		this.repository = repository;
-	}
-}
+// extends AbstractHandler {
+// // private final static Log log =
+// // LogFactory.getLog(AddEntityReferences.class);
+//
+// public final static String ID = PeopleRapPlugin.PLUGIN_ID
+// + ".addEntityReferences";
+// public final static ImageDescriptor DEFAULT_IMG_DESCRIPTOR = PeopleRapPlugin
+// .getImageDescriptor("icons/add.png");
+// public final static String DEFAULT_LABEL = "Add...";
+// public final static String PARAM_REFERENCING_JCR_ID =
+// "param.referencingJcrId";
+// public final static String PARAM_REFERENCED_JCR_ID = "param.referencedJcrId";
+//
+// /* DEPENDENCY INJECTION */
+// private PeopleService peopleService;
+// private Repository repository;
+//
+// public Object execute(final ExecutionEvent event) throws ExecutionException {
+//
+// String referencingJcrId = event.getParameter(PARAM_REFERENCING_JCR_ID);
+// String referencedJcrId = event.getParameter(PARAM_REFERENCED_JCR_ID);
+//
+// Session session = null;
+// try {
+// session = repository.login();
+// Node referencing = null;
+// if (referencingJcrId != null)
+// referencing = session.getNodeByIdentifier(referencingJcrId);
+//
+// Node referenced = null;
+// if (referencedJcrId != null)
+// referenced = session.getNodeByIdentifier(referencedJcrId);
+//
+// AddEntityReferenceWizard wizard = getCorrespondingWizard(event,
+// referencing, referenced);
+//
+// if (wizard == null) {
+// Dialog diag = new CreateEntityRefWithPositionDialog(
+// HandlerUtil.getActiveShell(event), "Create position",
+// repository, peopleService, referencing, referenced,
+// PeopleTypes.PEOPLE_ORG);
+// diag.open();
+// return null;
+// }
+//
+// WizardDialog dialog = new WizardDialog(
+// HandlerUtil.getActiveShell(event), wizard);
+// int result = dialog.open();
+//
+// if (result == WizardDialog.OK) {
+// IEditorPart iep = HandlerUtil.getActiveWorkbenchWindow(event)
+// .getActivePage().getActiveEditor();
+// if (iep != null && iep instanceof AbstractEntityEditor)
+// ((AbstractEntityEditor) iep).forceRefresh();
+// }
+// } catch (RepositoryException e) {
+// throw new PeopleException("unexpected JCR error while opening "
+// + "editor for newly created programm", e);
+// } finally {
+// JcrUtils.logoutQuietly(session);
+// }
+// return null;
+// }
+//
+// protected AddEntityReferenceWizard getCorrespondingWizard(
+// ExecutionEvent event, Node referencing, Node referenced)
+// throws RepositoryException {
+// if (referencing != null) {
+// if (referencing.isNodeType(PeopleTypes.PEOPLE_PERSON))
+// return null;
+// }
+// return null;
+// }
+//
+// /* DEPENDENCY INJECTION */
+// public void setPeopleService(PeopleService peopleService) {
+// this.peopleService = peopleService;
+// }
+//
+// public void setRepository(Repository repository) {
+// this.repository = repository;
+// }
+// }
