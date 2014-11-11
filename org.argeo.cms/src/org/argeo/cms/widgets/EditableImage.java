584c7365dd59bee939ed57154595faaed4475f64
Mathieu Baudier <mbaudier@argeo.org>
2014-11-11 19:12:28 +0000

    Introduce StyledControl and EditableImage
    git-svn-id: https://svn.argeo.org/connect/trunk@7438 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.cms/src/org/argeo/cms/widgets/EditableImage.java
+++ org.argeo.cms/src/org/argeo/cms/widgets/EditableImage.java
@@ -9,21 +9,20 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
-/** Editable text part displaying styled text. */
-public class EditableText extends StyledControl {
-	private static final long serialVersionUID = -6372283442330912755L;
+public class EditableImage extends StyledControl {
+	private static final long serialVersionUID = -5689145523114022890L;
 
-	public EditableText(Composite parent, int swtStyle) {
+	public EditableImage(Composite parent, int swtStyle) {
 		super(parent, swtStyle);
-		setLayout(CmsUtils.noSpaceGridLayout());
 	}
 
 	@Override
 	protected Control createControl(Composite box, String style) {
-		if (isEditing())
-			return createText(box, style);
-		else
+		if (isEditing()) {
+			return createImageChooser(box, style);
+		} else {
 			return createLabel(box, style);
+		}
 	}
 
 	protected Label createLabel(Composite box, String style) {
@@ -36,7 +35,7 @@ public class EditableText extends StyledControl {
 		return lbl;
 	}
 
-	protected Text createText(Composite box, String style) {
+	protected Control createImageChooser(Composite box, String style) {
 		final Text text = new Text(box, getStyle() | SWT.MULTI | SWT.WRAP);
 		GridData textLayoutData = CmsUtils.fillWidth();
 		// textLayoutData.minimumHeight = height;
@@ -44,27 +43,6 @@ public class EditableText extends StyledControl {
 		text.setData(STYLE, style);
 		text.setFocus();
 		return text;
-	}
-
-	// public void startEditing() {
-	// int height = child.getSize().y;
-	// String style = (String) child.getData(STYLE);
-	// clear(false);
-	// child = createText(style, height);
-	// }
-	//
-	// public void stopEditing() {
-	// String style = (String) child.getData(STYLE);
-	// clear(false);
-	// child = createLabel(style);
-	// }
-
-	public Text getAsText() {
-		return (Text) getControl();
-	}
-
-	public Label getAsLabel() {
-		return (Label) getControl();
 	}
 
 }
---
+++ org.argeo.cms/src/org/argeo/cms/widgets/StyledControl.java
@@ -0,0 +1,98 @@
+package org.argeo.cms.widgets;
+
+import org.argeo.cms.CmsConstants;
+import org.argeo.cms.CmsNames;
+import org.argeo.cms.CmsUtils;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+/** Editable text part displaying styled text. */
+public abstract class StyledControl extends Composite implements CmsConstants,
+		CmsNames {
+	private static final long serialVersionUID = -6372283442330912755L;
+	private Control control;
+
+	private Composite container;
+	private Composite box;
+
+	protected MouseListener mouseListener;
+
+	private Boolean editing = Boolean.FALSE;
+
+	public StyledControl(Composite parent, int swtStyle) {
+		super(parent, swtStyle);
+		setLayout(CmsUtils.noSpaceGridLayout());
+	}
+
+	protected abstract Control createControl(Composite box, String style);
+
+	protected Composite createBox(Composite parent) {
+		Composite container = new Composite(parent, SWT.INHERIT_DEFAULT);
+		container.setLayoutData(CmsUtils.fillWidth());
+		container.setLayout(CmsUtils.noSpaceGridLayout());
+		return container;
+	}
+
+	public Control getControl() {
+		return control;
+	}
+
+	protected synchronized Boolean isEditing() {
+		return editing;
+	}
+
+	public synchronized void startEditing() {
+		assert !isEditing();
+		editing = true;
+		// int height = child.getSize().y;
+		String style = (String) control.getData(STYLE);
+		clear(false);
+		control = createControl(box, style);
+	}
+
+	public synchronized void stopEditing() {
+		assert isEditing();
+		editing = false;
+		String style = (String) control.getData(STYLE);
+		clear(false);
+		control = createControl(box, style);
+	}
+
+	public void setStyle(String style) {
+		Object currentStyle = null;
+		if (control != null)
+			currentStyle = control.getData(STYLE);
+		if (currentStyle != null && currentStyle.equals(style))
+			return;
+
+		clear(true);
+		control = createControl(box, style);
+		// if (child == null || child instanceof Label)
+		// child = createLabel(style);
+		// else if (child instanceof Text)
+		// child = createText(style, child.getSize().y);
+		control.getParent().setData(STYLE, style + "_box");
+		control.getParent().getParent().setData(STYLE, style + "_container");
+	}
+
+	protected void clear(boolean deep) {
+		if (deep) {
+			for (Control control : getChildren())
+				control.dispose();
+			container = createBox(this);
+			box = createBox(container);
+		} else {
+			control.dispose();
+		}
+	}
+
+	public void setMouseListener(MouseListener mouseListener) {
+		if (this.mouseListener != null && control != null)
+			control.removeMouseListener(this.mouseListener);
+		this.mouseListener = mouseListener;
+		if (control != null && this.mouseListener != null)
+			control.addMouseListener(mouseListener);
+	}
+}
