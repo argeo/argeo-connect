698d5a276d6e07fa77decf224b59256ab01e33f8
Bruno Sinou <bsinou@argeo.org>
2015-02-27 13:16:54 +0000

    Introduce a maintenance service
    git-svn-id: https://svn.argeo.org/connect/trunk@7977 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ org.argeo.connect.people.core/src/org/argeo/connect/people/core/MaintenanceServiceImpl.java
@@ -0,0 +1,20 @@
+package org.argeo.connect.people.core;
+
+import org.argeo.connect.people.MaintenanceService;
+import org.argeo.connect.people.PeopleService;
+
+/**
+ * Default implementation of the maintenance and monitoring services in a People
+ * repository
+ */
+public class MaintenanceServiceImpl implements MaintenanceService {
+
+	private String pathToRepository = System.getProperty("user.dir");
+
+	private PeopleService peopleService;
+
+	public MaintenanceServiceImpl(PeopleService peopleService) {
+		this.peopleService = peopleService;
+	}
+
+}
---
+++ org.argeo.connect.people.specs/src/org/argeo/connect/people/MaintenanceService.java
@@ -0,0 +1,6 @@
+package org.argeo.connect.people;
+
+/** Provides method interfaces to maintain and monitor a people repository */
+public interface MaintenanceService {
+
+}
