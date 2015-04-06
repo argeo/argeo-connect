bcbde28cb52f47ad31d8feae0982d2b53529fd78
Bruno Sinou <bsinou@argeo.org>
2015-04-06 15:38:44 +0000

    Use an asynchronous job to perform the excel export
    git-svn-id: https://svn.argeo.org/connect/trunk@8053 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.connect.people.rap/src/org/argeo/connect/people/rap/commands/GetCalcExport.java
+++ org.argeo.connect.people.rap/src/org/argeo/connect/people/rap/commands/GetCalcExport.java
@@ -7,6 +7,10 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.jcr.Repository;
+import javax.jcr.Session;
+
+import org.argeo.ArgeoMonitor;
 import org.argeo.connect.people.PeopleException;
 import org.argeo.connect.people.rap.PeopleRapConstants;
 import org.argeo.connect.people.rap.PeopleRapPlugin;
@@ -14,11 +18,18 @@ import org.argeo.connect.people.rap.PeopleWorkbenchService;
 import org.argeo.connect.people.rap.exports.calc.ITableProvider;
 import org.argeo.connect.people.rap.exports.calc.RowsToCalcWriter;
 import org.argeo.connect.people.utils.CommonsJcrUtils;
+import org.argeo.eclipse.ui.EclipseArgeoMonitor;
 import org.argeo.eclipse.ui.specific.OpenFile;
 import org.argeo.eclipse.ui.workbench.CommandUtils;
+import org.argeo.jcr.JcrUtils;
+import org.argeo.security.ui.PrivilegedJob;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -28,7 +39,8 @@ import org.eclipse.ui.handlers.HandlerUtil;
  * must implement ICalcExtractProvider interface.
  */
 public class GetCalcExport extends AbstractHandler {
-	public final static String ID = PeopleRapPlugin.PLUGIN_ID + ".getCalcExport";
+	public final static String ID = PeopleRapPlugin.PLUGIN_ID
+			+ ".getCalcExport";
 	public final static String PARAM_EXPORT_ID = "param.exportId";
 
 	private final static DateFormat df = new SimpleDateFormat(
@@ -36,6 +48,7 @@ public class GetCalcExport extends AbstractHandler {
 
 	/* DEPENDENCY INJECTION */
 	private PeopleWorkbenchService peopleWorkbenchService;
+	private Repository repository;
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		String exportId = event.getParameter(PARAM_EXPORT_ID);
@@ -52,21 +65,9 @@ public class GetCalcExport extends AbstractHandler {
 				if ((provider).getColumnDefinition(exportId) == null)
 					return null;
 				else {
-					// Create file
-					File tmpFile = File
-							.createTempFile("people-extract", ".ods");
-					tmpFile.deleteOnExit();
-
-					// Effective generation
-					callCalcGenerator(provider, exportId, tmpFile);
-
-					// Open result file
-					Map<String, String> params = new HashMap<String, String>();
-					params.put(OpenFile.PARAM_FILE_NAME, getFileName(provider));
-					params.put(OpenFile.PARAM_FILE_URI,
-							"file://" + tmpFile.getAbsolutePath());
-					CommandUtils.callCommand(
-							peopleWorkbenchService.getOpenFileCmdId(), params);
+					new GenerateExtract(HandlerUtil.getActivePart(event)
+							.getSite().getShell().getDisplay(), repository,
+							provider, exportId).schedule();
 				}
 			} else
 				throw new PeopleException(iwp.toString()
@@ -97,8 +98,76 @@ public class GetCalcExport extends AbstractHandler {
 				provider.getColumnDefinition(exportId));
 	}
 
+	/** Privileged job that performs the extract asynchronously */
+	private class GenerateExtract extends PrivilegedJob {
+
+		private Display display;
+		private Repository repository;
+		private ITableProvider provider;
+		private String exportId;
+
+		public GenerateExtract(Display display, Repository repository,
+				ITableProvider provider, String exportId) {
+			super("Generating the export");
+			this.display = display;
+			this.repository = repository;
+			this.provider = provider;
+			this.exportId = exportId;
+
+		}
+
+		protected IStatus doRun(IProgressMonitor progressMonitor) {
+			Session session = null;
+			try {
+				ArgeoMonitor monitor = new EclipseArgeoMonitor(progressMonitor);
+				if (monitor != null && !monitor.isCanceled()) {
+					monitor.beginTask("Getting objects", -1);
+
+					session = repository.login();
+
+					// Create file
+					final File tmpFile = File.createTempFile("people-extract",
+							".ods");
+					tmpFile.deleteOnExit();
+
+					// Effective generation
+					callCalcGenerator(provider, exportId, tmpFile);
+
+					display.asyncExec(new Runnable() {
+
+						@Override
+						public void run() {
+							// Open result file
+							Map<String, String> params = new HashMap<String, String>();
+							params.put(OpenFile.PARAM_FILE_NAME,
+									getFileName(provider));
+							params.put(OpenFile.PARAM_FILE_URI, "file://"
+									+ tmpFile.getAbsolutePath());
+							CommandUtils.callCommand(
+									peopleWorkbenchService.getOpenFileCmdId(),
+									params);
+
+						}
+					});
+				}
+			} catch (Exception e) {
+				return new Status(IStatus.ERROR, PeopleRapPlugin.PLUGIN_ID,
+						"Unable to refresh tag and ML cache on " + repository,
+						e);
+			} finally {
+				JcrUtils.logoutQuietly(session);
+			}
+			return Status.OK_STATUS;
+		}
+	}
+
 	/* DEPENDENCY INJECTION */
-	public void setPeopleWorkbenchService(PeopleWorkbenchService peopleWorkbenchService) {
+	public void setPeopleWorkbenchService(
+			PeopleWorkbenchService peopleWorkbenchService) {
 		this.peopleWorkbenchService = peopleWorkbenchService;
+	}
+
+	public void setRepository(Repository repository) {
+		this.repository = repository;
 	}
 }
