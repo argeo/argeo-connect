e781a391162c6f1f468fcba39cdc70b254630d5f
Bruno Sinou <bsinou@argeo.org>
2015-06-25 09:05:28 +0000

    Fix date format
    git-svn-id: https://svn.argeo.org/connect/trunk@8222 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.connect.people.ui/src/org/argeo/connect/people/ui/PeopleUiConstants.java
+++ org.argeo.connect.people.ui/src/org/argeo/connect/people/ui/PeopleUiConstants.java
@@ -13,15 +13,16 @@ public interface PeopleUiConstants {
 	public final static String CRUD_DELETE = "Delete";
 
 	/* UI WIDGETS DATA KEYS */
-	// We often need to store an ordered list of arbitrary objects that goes together
+	// We often need to store an ordered list of arbitrary objects that goes
+	// together
 	// with the array of String that is displayed to the end user. Thus the
 	// selected object will be retrieved using this:
 	public final static String COMBO_BUSINESS_OBJECTS = "comboBusinessList";
 
 	// Default English date and numbers formats
 	public final static String DEFAULT_SHORT_DATE_FORMAT = "dd/MM/yyyy";
-	public final static String DEFAULT_DATE_FORMAT = "MMM, dd yyyy";
-	public final static String DEFAULT_DATE_TIME_FORMAT = "MMM, dd yyyy 'at' HH:mm";
+	public final static String DEFAULT_DATE_FORMAT = "MMM dd, yyyy";
+	public final static String DEFAULT_DATE_TIME_FORMAT = "MMM dd, yyyy 'at' HH:mm";
 	public final static String DEFAULT_NUMBER_FORMAT = "#,##0.0";
 
 	// Default column size for various tables and extracts
