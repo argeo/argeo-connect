29e6c3a3db6f0111c0189db3212cf698f2677c12
Bruno Sinou <bsinou@argeo.org>
2015-09-25 07:46:41 +0000

    Remove legacy user import from people web demo
    git-svn-id: https://svn.argeo.org/connect/trunk@8451 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.connect.people.web/src/org/argeo/connect/people/web/demo/DemoDataImport.java
+++ org.argeo.connect.people.web/src/org/argeo/connect/people/web/demo/DemoDataImport.java
@@ -3,15 +3,9 @@ package org.argeo.connect.people.web.demo;
 import java.util.Map;
 
 import javax.jcr.Node;
-import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.query.Query;
-import javax.jcr.query.QueryManager;
-import javax.jcr.query.QueryResult;
-import javax.jcr.query.qom.QueryObjectModel;
-import javax.jcr.query.qom.QueryObjectModelFactory;
-import javax.jcr.query.qom.Selector;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -21,48 +15,42 @@ import org.argeo.connect.people.PeopleNames;
 import org.argeo.connect.people.PeopleService;
 import org.argeo.connect.people.PeopleTypes;
 import org.argeo.connect.people.ResourceService;
-import org.argeo.connect.people.core.imports.GroupsCsvFileParser;
 import org.argeo.connect.people.core.imports.OrgCsvFileParser;
 import org.argeo.connect.people.core.imports.PersonCsvFileParser;
-import org.argeo.connect.people.core.imports.UsersCsvFileParser;
-import org.argeo.connect.people.utils.CommonsJcrUtils;
-import org.argeo.jcr.ArgeoNames;
-import org.argeo.jcr.ArgeoTypes;
 import org.argeo.jcr.JcrUtils;
 import org.argeo.security.UserAdminService;
-import org.argeo.util.CsvParserWithLinesAsMap;
 import org.springframework.core.io.Resource;
 
 /** Fill the repository in a demo context */
 public class DemoDataImport implements PeopleConstants {
 	private final static Log log = LogFactory.getLog(DemoDataImport.class);
 
-	private PeopleService peopleService;
+	// private PeopleService peopleService;
 
 	/** Imports demo data */
 	public void doImport(PeopleService peopleService,
 			UserAdminService userAdminService, Session adminSession,
 			Map<String, Resource> demoData) {
 		try {
-			this.peopleService = peopleService;
+			// this.peopleService = peopleService;
 
 			// Images
 			Resource dummyImageFolder = demoData.get("demoImages");
 
 			Resource resource;
 
-			// User & group management
-			resource = demoData.get("userFile");
-			// Force the import of org.argeo.util.CsvParserWithLinesAsMap
-			CsvParserWithLinesAsMap parser = new UsersCsvFileParser(
-					adminSession, peopleService, userAdminService);
-			parser.parse(resource.getInputStream(), "UTF-8");
-
-			createUserGroups(adminSession);
-
-			resource = demoData.get("groupFile");
-			new GroupsCsvFileParser(adminSession, peopleService).parse(
-					resource.getInputStream(), "UTF-8");
+			// // User & group management
+			// resource = demoData.get("userFile");
+			// // Force the import of org.argeo.util.CsvParserWithLinesAsMap
+			// CsvParserWithLinesAsMap parser = new UsersCsvFileParser(
+			// adminSession, peopleService, userAdminService);
+			// parser.parse(resource.getInputStream(), "UTF-8");
+			//
+			// createUserGroups(adminSession);
+			//
+			// resource = demoData.get("groupFile");
+			// new GroupsCsvFileParser(adminSession, peopleService).parse(
+			// resource.getInputStream(), "UTF-8");
 
 			// Entities
 			resource = demoData.get("orgFile");
@@ -126,27 +114,27 @@ public class DemoDataImport implements PeopleConstants {
 
 	}
 
-	private void createUserGroups(Session adminSession)
-			throws RepositoryException {
-
-		QueryManager queryManager = adminSession.getWorkspace()
-				.getQueryManager();
-		QueryObjectModelFactory factory = queryManager.getQOMFactory();
-		Selector source = factory.selector(ArgeoTypes.ARGEO_USER_PROFILE,
-				ArgeoTypes.ARGEO_USER_PROFILE);
-		QueryObjectModel query = factory.createQuery(source, null, null, null);
-		QueryResult result = query.execute();
-
-		NodeIterator nit = result.getNodes();
-		while (nit.hasNext()) {
-			Node currProfile = nit.nextNode();
-			String username = CommonsJcrUtils.get(currProfile,
-					ArgeoNames.ARGEO_USER_ID);
-
-			// TODO remove hard coded default users names
-			if (!("root".equals(username) || "demo".equals(username)))
-				peopleService.getUserManagementService()
-						.createDefaultGroupForUser(adminSession, username);
-		}
-	}
+	// private void createUserGroups(Session adminSession)
+	// throws RepositoryException {
+	//
+	// QueryManager queryManager = adminSession.getWorkspace()
+	// .getQueryManager();
+	// QueryObjectModelFactory factory = queryManager.getQOMFactory();
+	// Selector source = factory.selector(ArgeoTypes.ARGEO_USER_PROFILE,
+	// ArgeoTypes.ARGEO_USER_PROFILE);
+	// QueryObjectModel query = factory.createQuery(source, null, null, null);
+	// QueryResult result = query.execute();
+	//
+	// NodeIterator nit = result.getNodes();
+	// while (nit.hasNext()) {
+	// Node currProfile = nit.nextNode();
+	// String username = CommonsJcrUtils.get(currProfile,
+	// ArgeoNames.ARGEO_USER_ID);
+	//
+	// // // TODO remove hard coded default users names
+	// // if (!("root".equals(username) || "demo".equals(username)))
+	// // peopleService.getUserManagementService()
+	// // .createDefaultGroupForUser(adminSession, username);
+	// }
+	// }
 }
