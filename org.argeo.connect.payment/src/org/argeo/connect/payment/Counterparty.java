2ab87d68a19c30058f73903699b6de6f52e07dec
Mathieu Baudier <mbaudier@argeo.org>
2015-11-13 17:42:10 +0000

    - Use Argeo TP extras v2.1.7 - Delete unused Connect Files - Introduce Connect Payment
    git-svn-id: https://svn.argeo.org/connect/trunk@8573 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ org.argeo.connect.payment/src/org/argeo/connect/payment/Counterparty.java
@@ -0,0 +1,5 @@
+package org.argeo.connect.payment;
+
+public interface Counterparty {
+
+}
---
+++ org.argeo.connect.payment/src/org/argeo/connect/payment/stripe/StripeTransaction.java
@@ -0,0 +1,57 @@
+package org.argeo.connect.payment.stripe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.argeo.connect.payment.Counterparty;
+import org.argeo.connect.payment.Transaction;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import com.stripe.net.RequestOptions;
+
+public class StripeTransaction implements Transaction {
+
+	@Override
+	public Object getBusinessId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Counterparty getSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Counterparty getTarget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/** from https://stripe.com/docs/api?lang=java */
+	public void test() {
+		try {
+			Stripe.apiKey = "sk_test_rgydDhfOG9cu1y2N2PDKs0Pd";
+
+			Map<String, Object> chargeParams = new HashMap<String, Object>();
+			chargeParams.put("amount", 400);
+			chargeParams.put("currency", "eur");
+			chargeParams.put("source", "tok_175Gk1DD4YBBbu5ao8DFl7W6"); // obtained
+																		// with
+																		// Stripe.js
+			chargeParams.put("description", "Charge for test@example.com");
+
+			RequestOptions options = RequestOptions.builder()
+					.setIdempotencyKey("5NxN9SYyYhwfPCG5").build();
+
+			Charge.create(chargeParams, options);
+		} catch (StripeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
