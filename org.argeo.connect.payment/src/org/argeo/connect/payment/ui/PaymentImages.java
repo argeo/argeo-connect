9be890b84d54bd5b60348fd82da0f64caa72a6d0
Bruno Sinou <bsinou@argeo.org>
2016-01-06 16:41:24 +0000

    Remove unused ability to be started as a plugin
    git-svn-id: https://svn.argeo.org/connect/trunk@8767 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.connect.payment/src/org/argeo/connect/payment/ui/PaymentImages.java
+++ org.argeo.connect.payment/src/org/argeo/connect/payment/ui/PaymentImages.java
@@ -1,8 +1,7 @@
 package org.argeo.connect.payment.ui;
 
-import org.eclipse.swt.graphics.Image;
 
 public class PaymentImages {
-	public final static Image ICON_LOCK = PaymentUiPlugin.getImageDescriptor(
-			"icons/lock.png").createImage();
+	// public final static Image ICON_LOCK = PaymentUiPlugin.getImageDescriptor(
+	// "icons/lock.png").createImage();
 }
