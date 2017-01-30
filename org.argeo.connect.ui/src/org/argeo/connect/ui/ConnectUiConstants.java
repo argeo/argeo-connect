5b70389585a320c317ddf4fd981214673e776a58
bsinou <bsinou@argeo.org>
2017-01-30 23:41:30 +0100

    Small enhancements

--- org.argeo.connect.ui/src/org/argeo/connect/ui/ConnectUiConstants.java
+++ org.argeo.connect.ui/src/org/argeo/connect/ui/ConnectUiConstants.java
@@ -6,30 +6,32 @@ package org.argeo.connect.ui;
  * TODO handle this cleanly.
  */
 public interface ConnectUiConstants {
+	// Nb of millisecond between 2 requests in the delayed filtered text
+	int SEARCH_TEXT_DELAY = 800;
+
 	// Characters that must be replaced by their codes for RWT
-	public static String NB_SPACE = "&#160;";
-	public static String NB_DOUBLE_SPACE = "&#160;&#160;";
-	public static String AMPERSAND = "&#38;";
+	String NB_SPACE = "&#160;";
+	String NB_DOUBLE_SPACE = "&#160;&#160;";
+	String AMPERSAND = "&#38;";
 
 	// Crud ID & Default label to manage list items among others.
-	public final static String CRUD_CREATE = "Create";
-	public final static String CRUD_VIEW = "View";
-	public final static String CRUD_EDIT = "Edit";
-	public final static String CRUD_DELETE = "Delete";
+	String CRUD_CREATE = "Create";
+	String CRUD_VIEW = "View";
+	String CRUD_EDIT = "Edit";
+	String CRUD_DELETE = "Delete";
 
 	/* UI WIDGETS DATA KEYS */
 	// We often need to store an ordered list of arbitrary objects that goes
-	// together
-	// with the array of String that is displayed to the end user. Thus the
-	// selected object will be retrieved using this:
-	public final static String COMBO_BUSINESS_OBJECTS = "comboBusinessList";
+	// together with the array of String that is displayed to the end user. Thus
+	// the selected object will be retrieved using this:
+	String COMBO_BUSINESS_OBJECTS = "comboBusinessList";
 
 	// Default English date and numbers formats
-	public final static String DEFAULT_SHORT_DATE_FORMAT = "dd/MM/yyyy";
-	public final static String DEFAULT_DATE_FORMAT = "MMM dd, yyyy";
-	public final static String DEFAULT_DATE_TIME_FORMAT = "MMM dd, yyyy 'at' HH:mm";
-	public final static String DEFAULT_NUMBER_FORMAT = "#,##0.0";
+	String DEFAULT_SHORT_DATE_FORMAT = "dd/MM/yyyy";
+	String DEFAULT_DATE_FORMAT = "MMM dd, yyyy";
+	String DEFAULT_DATE_TIME_FORMAT = "MMM dd, yyyy 'at' HH:mm";
+	String DEFAULT_NUMBER_FORMAT = "#,##0.0";
 
 	// Default column size for various tables and extracts
-	public final static int DEFAULT_COLUMN_SIZE = 120;
+	int DEFAULT_COLUMN_SIZE = 120;
 }
