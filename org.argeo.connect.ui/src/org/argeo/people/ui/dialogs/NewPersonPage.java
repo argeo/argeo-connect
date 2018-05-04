f6bb467fa150df2874af2b40cf286e9c2f6514f5
Mathieu Baudier <mbaudier@argeo.org>
2018-05-04 18:05:31 +0200

    Introduce new person wizard page

---
+++ org.argeo.connect.ui/src/org/argeo/people/ui/dialogs/NewPersonPage.java
@@ -0,0 +1,18 @@
+package org.argeo.people.ui.dialogs;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.widgets.Composite;
+
+public class NewPersonPage extends WizardPage {
+
+	protected NewPersonPage(String pageName) {
+		super(pageName);
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
