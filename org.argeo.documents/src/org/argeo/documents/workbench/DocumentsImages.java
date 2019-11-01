f25f5c755f043c8423b280506fea6fea69b12a68
Mathieu Baudier <mbaudier@argeo.org>
2019-11-01 10:51:27 +0100

    Remove Eclipse 3 support.

--- org.argeo.documents/src/org/argeo/documents/workbench/DocumentsImages.java
+++
@@ -1,7 +0,0 @@
-package org.argeo.documents.workbench;
-
-/** Shared icons for the file system RAP workbench */
-@Deprecated
-class DocumentsImages {
-	public final static String BASE_PATH = "/theme/icons/types/";
-}
--- org.argeo.media/src/org/argeo/media/MediaAppService.java
+++
@@ -1,5 +0,0 @@
-package org.argeo.media;
-
-/** Provides method interfaces to manage a film repository */
-public interface MediaAppService {
-}
