0e3715228378a53724a7367695a5b5d9ddcf7121
Mathieu Baudier <mbaudier@argeo.org>
2020-01-04 12:14:39 +0100

    Integrate DocBook support and Example from Argeo Commons.

---
+++ org.argeo.connect.api/src/org/argeo/docbook/jcr/DocBookModel.java
@@ -0,0 +1,35 @@
+package org.argeo.docbook.jcr;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.jcr.ImportUUIDBehavior;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+public class DocBookModel {
+	private Session session;
+
+	public DocBookModel(Session session) {
+		super();
+		this.session = session;
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
+	}
+
+	public void importXml(String path, InputStream in) throws RepositoryException, IOException {
+		long begin = System.currentTimeMillis();
+		session.importXML(path, in, ImportUUIDBehavior.IMPORT_UUID_COLLISION_REPLACE_EXISTING);
+		long duration = System.currentTimeMillis() - begin;
+//		if (log.isTraceEnabled())
+//			log.trace("Imported " + path + " in " + duration + " ms");
+
+	}
+
+	public void exportXml(String path, OutputStream out) throws RepositoryException, IOException {
+		session.exportDocumentView(path, out, true, false);
+	}
+}
---
+++ org.argeo.connect.api/src/org/argeo/docbook/jcr/DocBookNames.java
@@ -0,0 +1,16 @@
+package org.argeo.docbook.jcr;
+
+public interface DocBookNames {
+	// ELEMENTS
+	public final static String DBK_ARTICLE = "dbk:article";
+	public final static String DBK_PARA = "dbk:para";
+	public final static String DBK_SECTION = "dbk:section";
+	public final static String DBK_MEDIAOBJECT = "dbk:mediaobject";
+
+	// ATTRIBUTES
+	// TODO centralise
+	public final static String JCR_XMLTEXT = "jcr:xmltext";
+	public final static String JCR_XMLCHARACTERS = "jcr:xmlcharacters";
+	
+	public final static String DBK_ROLE = "dbk:role";
+}
